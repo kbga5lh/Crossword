dffb481ba50d15f915994c6587cdfe46d9ee46e5
Vladislav Gladkiy <kbga5lh@gmail.com>
2020-04-15 19:12:12 +0300

    Update

---
+++ Generation/MainCrosswordGenerationPage.xaml.cs
@@ -0,0 +1,31 @@
+using System.Windows.Controls;
+
+namespace CrosswordApp
+{
+    public partial class MainCrosswordGenerationPage : Page
+    {
+        readonly WordsPage wordsPage = new WordsPage();
+        
+        public MainCrosswordGenerationPage()
+        {
+            InitializeComponent();
+            
+            Content = wordsPage;
+        }
+
+        public void ToWordsPage()
+        {
+            Content = wordsPage;
+        }
+        
+        public void ToCrosswordPage(CrosswordPage crosswordPage)
+        {
+            Content = crosswordPage;
+        }
+        
+        public void ToMenuPage()
+        {
+            
+        }
+    }
+}
